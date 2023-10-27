99711ab59737ec0f0e7ec0108b6f4b9ebc73dcd2
isabellagoes <isabellagoes.igs@gmail.com>
2023-10-27 20:36:38 -0300

    Aula Laços 27 de outubro e implementação do TryParse em alternativa ao ConvertToInt

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿int numero;
+int numeroPar = 0;
+ 
+do
+{
+    Console.Write("Digite um número inteiro e positivo: ");
+    //numero = ConvertToInt32(Console.ReadLine());
+    Int32.TryParse(Console.ReadLine(), out numero);
+} 
+while (numero <= 0);
+ 
+while (numeroPar <= numero)
+{
+    Console.Write($"{numeroPar} ");
+    numeroPar += 2;
+}
